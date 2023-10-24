0539424e24f970297d33daf5bee98b3af1b45734
chayapatr <chayapatr.arc@gmail.com>
2023-10-24 17:09:36 +0700

    style

--- postag/postag.py
+++ postag/postag.py
@@ -1,8 +1,17 @@
 from pythainlp.tokenize import word_tokenize
 from pythainlp.tag import pos_tag
 
-x = "./scrape/chayapatr/apollo-guidance-computer.txt"
-with open(x, encoding="utf-8") as f:
+original = "./scrape/chayapatr/apollo-guidance-computer.txt"
+save = "./tagged.txt"
+with open(original, encoding="utf-8") as f:
     tokenized = word_tokenize(f.read())
     tagged = pos_tag(tokenized)
-    print(tagged)
+    with open(save, "w", encoding="utf-8") as g:
+        w = []
+        for i in tagged:
+            if i[0] not in ["\n", " "]:
+                w.append(f"{i[0]}_{i[1]}")
+            else:
+                print("hit", i)
+                w.append(i[0])
+        g.write("|".join(w))
